bcc9e4f8da25ec251c1a778151634deee705fc70
ChiThang-50Cent <chithang.nydo@gmail.com>
2023-11-30 16:28:13 +0700

    done

--- test.py
+++ test.py
@@ -4,21 +4,25 @@ import time
 import cv2 as cv
 import multiprocessing as mp
 
+from flask import Flask, Response
+
 def put_bath_to_queue(queue):
     try:
         vid = cv.VideoCapture('rtsp://admin:admin@192.168.1.10:1935')
         vid_batch = []
-        print("__Start stream__")
+
+        print('Into stream')
+        
         while(vid.isOpened()):
             ret, frame = vid.read()
             if ret:
                 vid_batch.append(frame)
-                cv.imshow('Home', frame)
-                cv.waitKey(1)
+                # cv.imshow('Home', frame)
+                # cv.waitKey(1)
             else:
                 break        
             if len(vid_batch) >= 24:
-                queue.put(vid_batch)
+                queue.put((vid_batch, time.ctime()))
                 vid_batch = []
 
             if queue.qsize() > MAX_SIZE:
@@ -41,7 +45,7 @@ def detection(queue):
     has_obj = False
 
     while True:
-        vid_batch = queue.get()
+        vid_batch, c_time = queue.get()
         vid_batch = vid_batch[::3]
         res = model_onnx(vid_batch)
 
@@ -70,7 +74,24 @@ def detection(queue):
                 res = cv.imwrite(f'./out/{name}.jpg', img_)
 
                 if res:
-                    bot.send_noti(f'./out/{name}.jpg', time.ctime())
+                    bot.send_noti(f'./out/{name}.jpg', c_time)
+
+app = Flask(__name__)
+
+def get_frame(queue):
+    while True:
+        video_batch, _ = queue.get()
+        for frame in video_batch:
+            ret, buffer = cv.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            
+@app.route('/')
+def video_stream():
+    return Response(get_frame(queue), mimetype='multipart/x-mixed-replace; boundary=frame')
+    
 
 if __name__ == "__main__":
 
@@ -81,4 +102,6 @@ if __name__ == "__main__":
 
     p1.start()
     p2.start()
+
+    app.run()
 
